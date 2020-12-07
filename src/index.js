aa8efa8763dfebf2787c18af1a4d660a90d6ed7f
Alessandro Ramos <toprime13@gmail.com>
2020-12-06 21:39:15 -0300

    removido alert

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './componentes/App';
 
-function Aviso() {
-    alert('Aviso!!!\nCalculadora com possíveis bugs.\nCaso tenha algum, avise-me.')
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
-Aviso()
